3daa2b5fc93f2374dc0d27b2319f4437d31ddeec
Darek Lyssy <darek.lyss@gmail.com>
2018-10-05 20:09:45 +0200

    Second commit

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const express = require('express');
 
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 const app = express();
-const publicPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicPath));
 
